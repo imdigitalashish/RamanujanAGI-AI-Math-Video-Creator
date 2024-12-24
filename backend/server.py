7d7916d12bd38f495249e5ed3d7be5c1440dbaa2
imdigitalashish <imdigitalashish@gmail.com>
2024-12-24 22:33:03 +0530

    Implemented Agentic AI

--- backend/server.py
+++ backend/server.py
@@ -6,9 +6,27 @@ import uuid
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
+from pydantic import BaseModel
+from pydantic_ai import Agent
+import ast
+
 import re
+import asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+class ManimCode(BaseModel):
+    code: str
+
+agent = Agent(
+    "gemini-2.0-flash-exp",
+    result_type=ManimCode,
+    system_prompt="Write only the Raw Manim code nothing else to create an intuitive animation for",
+)
 
 app.mount("/directory", StaticFiles(directory=Path("directory"), html=True), name="directory")
 
@@ -52,20 +70,27 @@ def sanitize_code_output(code_output: str) -> str:
         return code_output  # Return as is if no indicators are found
 
 
-@app.get("/get_the_video")
-def getTheVideo(prompt: str):
-    prompt = "Write only the Raw Manim code nothing else to create an intuitive animation for " + prompt
 
-    response = client.chat.completions.create(
-        model="gpt-4o",  # Replace with your desired model name
-        messages=[{"role": "user", "content": prompt}]
-    )
-    manim_code = response.choices[0].message.content
-    print(manim_code)
+
+@app.get("/get_the_video")
+async def getTheVideo(prompt: str):
+    prompt = "Write only the Raw Manim code intuitive animations for " + prompt
+
+    # response = client.chat.completions.create(
+    #     model="gpt-4o",  # Replace with your desired model name
+    #     messages=[{"role": "user", "content": prompt}]
+    # )
+
+    response_agent = await agent.run(prompt)
+    print(response_agent.data.code.encode().decode('unicode_escape'))
+
+    
+    manim_code = response_agent.data.code
+    # print(manim_code)
 
     filename = f"{uuid.uuid4()}"
     with open("./"+filename+".py", "w") as file:
-        file.write(sanitize_code_output(manim_code))
+        file.write(str(manim_code.replace("\\n", "\n")).encode().decode('unicode_escape'))
 
     print(f"Manim code saved to {filename}")
 
